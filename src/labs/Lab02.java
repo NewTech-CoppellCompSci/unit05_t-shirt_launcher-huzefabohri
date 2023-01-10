b403fd2e40b3bc613277ec05fa67dfec4355c2dd
huzefabohri <hbohri@newtech.coppellisd.com>
2023-01-10 17:00:26 -0600

    1

--- src/labs/Lab02.java
+++ src/labs/Lab02.java
@@ -13,30 +13,101 @@ public class Lab02 {
 
 	}
 
-
 	public static void problem1() {
 		
 		
+		
+		Scanner inKey = new Scanner(System.in);
+		System.out.print("Enter a postive integer: ");
+		int num = inKey.nextInt();
+		System.out.print("Enter another positive integer: ");
+		int num2 = inKey.nextInt();
+		
+		double max = Math.max(num, num2);
+		double min = Math.min(num, num2);
+		
+		double scope = max - min + 1;
+		int rand = 0;	
+		for(int i = 0 ; i < 100; i++) {
+			
+			rand = (int) (Math.random() * scope) + ((int)min);
+			System.out.print(rand + " ");
+			
+		}
+	System.out.println();
 	}
 
 
 	public static void problem2() {
 		
+		System.out.println();
 		
+		Scanner inKey = new Scanner(System.in);
+		System.out.println("Enter the radius of a cylinder: \n");
+		double radius = inKey.nextDouble();
+		System.out.println("Enter the height of the cylinder: \n");
+		double height = inKey.nextDouble();
+		
+		double circ = height * Math.PI * Math.pow(radius, 2);
+		
+			System.out.println(circ);
 	}
 
 
 	public static void problem3() {
 		
+		Scanner inKey = new Scanner(System.in);
+		System.out.println("Enter x1: \n");
+		double x1 = inKey.nextDouble();
+		
+		System.out.println("Enter y1: \n");
+		double y1 = inKey.nextDouble();
+		
+		System.out.println("Enter x2: \n");
+		double x2 = inKey.nextDouble();
+		
+		System.out.println("Enter y2: \n");
+		double y2 = inKey.nextDouble();
+		
+		double diff = x2 - x1;
+		double diff1 = y2 - y1;
+		
+		double x = Math.pow(diff, 2);
+		
+		double y = Math.pow(diff1, 2);
+		
+		double total = x + y;
+		
+		double ans = Math.sqrt(total);
+		
+		System.out.print("Distance between points = " + ans + "\n");
 		
 	}
 
 	
+		public static void problem4() {
+			Scanner inKey = new Scanner(System.in);
+			
+			System.out.println("Enter a-value: ");
+			double a = inKey.nextDouble();
+		
+			System.out.println("Enter b-value: ");
+			double b = inKey.nextDouble();
+			
+			System.out.println("Enter c-value: ");
+			double c = inKey.nextDouble();
 	
-	public static void problem4() {
+			double first = -b + Math.sqrt(Math.pow(b, 2) - 4 * (a) * (c));
+			double second = -b - Math.sqrt(Math.pow(b, 2) - 4 * (a) * (c));
+			double third = 2 * a;
+			
+			double x1 = first / third; 
+			double x2 = second / third;
+			
+			System.out.println("x1 = " + x1 + "\n");
+			System.out.println("x2 = " + x2 + "\n");
 		
-		
-	}
+		}
 
 
 
--- src/performance_task/Performance_Task.java
+++ src/performance_task/Performance_Task.java
@@ -1,10 +1,54 @@
 package performance_task;
 
+import java.util.Scanner;
+
 public class Performance_Task {
 
 	public static void main(String[] args) {
 		
+			
+			
+			System.out.println("Alright, lets do a T - SHIRT LAUNCHER!!!!");
+			
+			System.out.println();
+			
+			Scanner inKey = new Scanner(System.in);
+			
+			System.out.println("Enter the launch velocity (m/s): \n");
+			double vel = inKey.nextDouble();	
+			
+			System.out.println("Enter the lauch angle (degrees): \n");
+			double angle = inKey.nextDouble();
+			
+			double xpos = 0;
+			double ypos = 1;
+			int time = 0;
+			boolean ground = true;
+			
+			System.out.println();
+			System.out.println("Projectile's path: \n");
+			
+			while (ground) {
+				
+			xpos = Math.cos(Math.toRadians(angle)) * time * vel;
+			ypos = Math.sin(Math.toRadians(angle)) * time * vel - 0.5 * 9.8 * Math.pow(time,2);
+				
+				if(ypos<0) {
+					ground = false;
+					ypos = 0;
+				
+				}
+			System.out.println("Time: " + time + "\n");
+				System.out.println("x-pos: " + xpos + "m \n");
+				System.out.println("y-pos: " + ypos + "m \n");
+
+				time++;
+				
+				
+			}
 		
+			
+			
 		
 		}
 	
