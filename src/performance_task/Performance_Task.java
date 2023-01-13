815b4e5e6e354f6f244407eea2da302761ea494f
huzefabohri <hbohri@newtech.coppellisd.com>
2023-01-13 17:30:02 -0600

    234567890-=

--- src/performance_task/Performance_Task.java
+++ src/performance_task/Performance_Task.java
@@ -20,24 +20,34 @@ public class Performance_Task {
 			System.out.println("Enter the lauch angle (degrees): \n");
 			double angle = inKey.nextDouble();
 			
+			// Asking for the users input on velocity and angle ^^
+			
 			double xpos = 0;
 			double ypos = 1;
 			int time = 0;
 			boolean ground = true;
 			
+			// Variables to print the code
+			
 			System.out.println();
 			System.out.println("Projectile's path: \n");
 			
+			// The while loop to make the launcher actually launch
+			
 			while (ground) {
-				
+			
+			//Math methods to calculate y and x
 			xpos = Math.cos(Math.toRadians(angle)) * time * vel;
 			ypos = Math.sin(Math.toRadians(angle)) * time * vel - 0.5 * 9.8 * Math.pow(time,2);
 				
+				//If statement for the boolean - so that the loop will stop once y = 0
 				if(ypos<0) {
 					ground = false;
 					ypos = 0;
 				
 				}
+			
+			//Printing the numbers	
 			System.out.println("Time: " + time + "\n");
 				System.out.println("x-pos: " + xpos + "m \n");
 				System.out.println("y-pos: " + ypos + "m \n");
